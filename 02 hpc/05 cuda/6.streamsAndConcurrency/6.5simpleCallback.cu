8cbcff64d2e8613a055530567705ad7863e43390
Jinbo <jinboom@foxmail.com>
2025-01-27 14:44:50 +0800

    feat(cuda): Stream callback

--- 02 hpc/05 cuda/6.streamsAndConcurrency/6.5simpleCallback.cu
+++ 02 hpc/05 cuda/6.streamsAndConcurrency/6.5simpleCallback.cu
@@ -1,13 +1,11 @@
-#include "../common/common.h"
-#include <stdio.h>
 #include <cuda_runtime.h>
+#include <stdio.h>
+#include "../common/common.h"
 
-/*
- * An example of using CUDA callbacks to trigger work on the host after the
- * completion of asynchronous work on the device. In this example, n_streams
- * CUDA streams are created and 4 kernels are launched asynchronously in each.
- * Then, a callback is added at the completion of those asynchronous kernels
- * that prints diagnostic information.
+/**
+ * 此示例展示了如何使用CUDA回调在设备上的异步工作完成后触发主机上的工作。
+ * 在此示例中，创建了n_streams个CUDA流，并在每个流中异步启动了4个内核。
+ * 然后，在这些异步内核完成后添加一个回调，用于打印诊断信息。
  */
 
 #define N 100000
@@ -22,7 +20,7 @@ __global__ void kernel_1()
 {
     double sum = 0.0;
 
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         sum = sum + tan(0.1) * tan(0.1);
     }
@@ -32,7 +30,7 @@ __global__ void kernel_2()
 {
     double sum = 0.0;
 
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         sum = sum + tan(0.1) * tan(0.1);
     }
@@ -42,17 +40,18 @@ __global__ void kernel_3()
 {
     double sum = 0.0;
 
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         sum = sum + tan(0.1) * tan(0.1);
     }
+
 }
 
 __global__ void kernel_4()
 {
     double sum = 0.0;
 
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         sum = sum + tan(0.1) * tan(0.1);
     }
@@ -62,7 +61,8 @@ int main(int argc, char **argv)
 {
     int n_streams = NSTREAM;
 
-    if (argc > 1) n_streams = atoi(argv[1]);
+    if (argc > 1)
+        n_streams = atoi(argv[1]);
 
     int dev = getGPUId();
     cudaDeviceProp deviceProp;
@@ -76,8 +76,9 @@ int main(int argc, char **argv)
     {
         if (deviceProp.concurrentKernels == 0)
         {
-            printf("> GPU does not support concurrent kernel execution (SM 3.5 "
-                   "or higher required)\n");
+            printf(
+                "> GPU does not support concurrent kernel execution (SM 3.5 "
+                "or higher required)\n");
             printf("> CUDA kernel runs will be serialized\n");
         }
         else
@@ -87,27 +88,26 @@ int main(int argc, char **argv)
         }
     }
 
-    printf("> Compute Capability %d.%d hardware with %d multi-processors\n",
-           deviceProp.major, deviceProp.minor, deviceProp.multiProcessorCount);
+    printf("> Compute Capability %d.%d hardware with %d multi-processors\n", deviceProp.major, deviceProp.minor,
+           deviceProp.multiProcessorCount);
 
     // set up max connectioin
-    char * iname = (char *)"CUDA_DEVICE_MAX_CONNECTIONS";
-    setenv (iname, "8", 1);
-    char *ivalue =  getenv (iname);
-    printf ("> %s = %s\n", iname, ivalue);
-    printf ("> with streams = %d\n", n_streams);
+    char *iname = (char *)"CUDA_DEVICE_MAX_CONNECTIONS";
+    setenv(iname, "8", 1);
+    char *ivalue = getenv(iname);
+    printf("> %s = %s\n", iname, ivalue);
+    printf("> with streams = %d\n", n_streams);
 
     // Allocate and initialize an array of stream handles
-    cudaStream_t *streams = (cudaStream_t *) malloc(n_streams * sizeof(
-                                cudaStream_t));
+    cudaStream_t *streams = (cudaStream_t *)malloc(n_streams * sizeof(cudaStream_t));
 
-    for (int i = 0 ; i < n_streams ; i++)
+    for (int i = 0; i < n_streams; i++)
     {
         CHECK(cudaStreamCreate(&(streams[i])));
     }
 
-    dim3 block (1);
-    dim3 grid  (1);
+    dim3 block(1);
+    dim3 grid(1);
     cudaEvent_t start_event, stop_event;
     CHECK(cudaEventCreate(&start_event));
     CHECK(cudaEventCreate(&stop_event));
@@ -123,8 +123,7 @@ int main(int argc, char **argv)
         kernel_2<<<grid, block, 0, streams[i]>>>();
         kernel_3<<<grid, block, 0, streams[i]>>>();
         kernel_4<<<grid, block, 0, streams[i]>>>();
-        CHECK(cudaStreamAddCallback(streams[i], my_callback,
-                    (void *)(stream_ids + i), 0));
+        CHECK(cudaStreamAddCallback(streams[i], my_callback, (void *)(stream_ids + i), 0));
     }
 
     CHECK(cudaEventRecord(stop_event, 0));
@@ -132,11 +131,10 @@ int main(int argc, char **argv)
 
     float elapsed_time;
     CHECK(cudaEventElapsedTime(&elapsed_time, start_event, stop_event));
-    printf("Measured time for parallel execution = %.3fs\n",
-           elapsed_time / 1000.0f);
+    printf("Measured time for parallel execution = %.3fs\n", elapsed_time / 1000.0f);
 
     // release all stream
-    for (int i = 0 ; i < n_streams ; i++)
+    for (int i = 0; i < n_streams; i++)
     {
         CHECK(cudaStreamDestroy(streams[i]));
     }
--- 02 hpc/05 cuda/readme.md
+++ 02 hpc/05 cuda/readme.md
@@ -1288,3 +1288,28 @@ cudaEventInterprocess
 
 参考代码 6.4asyncAPI.cu
 
+### 流回调
+
+流回调是另一种可以到CUDA流中排列等待的操作。一旦流回调之前所有的流操作全部完成，被流回调指定的主机端函数就会被CUDA运行时所调用。
+
+流回调是一种特别的技术，有点像是事件的函数，这个回调函数被放入流中，当其前面的任务都完成了，就会调用这个函数，但是比较特殊的是，在回调函数中，需要遵守下面的规则
+
+- 回调函数中不可以调用CUDA的API
+- 不可以执行同步
+
+流函数有特殊的参数规格，必须写成下面形式参数的函数;
+
+```cpp
+void CUDART_CB my_callback(cudaStream_t stream, cudaError_t status, void *data) {
+    printf("callback from stream %d\n", *((int *)data));
+}
+```
+
+然后使用：
+
+```cpp
+cudaError_t cudaStreamAddCallback(cudaStream_t stream,cudaStreamCallback_t callback, void *userData, unsigned int flags);
+```
+
+加入流中。
+
